fec52bf64f4b15811066ee057ee837000c9f82ff
ADITYAPIMPALE11127 <pimpaleaditya2@gmail.com>
2024-09-16 19:20:47 +0530

    updated portfolio

---
+++ data.js
@@ -0,0 +1,70 @@
+const projectData = [
+    {
+        id: 1,
+        title: "Snap Search",
+        description: "An image gallery app using React and the Pixabay API.",
+        image: "images/P2.jpg",
+        link: "https://snappy1635.netlify.app/",
+        github: "https://github.com/ADITYAPIMPALE11127/image-gallery-react--1"
+    },
+    {
+        id: 2,
+        title: "Bulk SMS & Email Service",
+        description: "A web service for efficient bulk SMS and email delivery.",
+        image: "images/P3.jpg",
+        github: "https://github.com/ADITYAPIMPALE11127/Bulk-SMS-and-email-sending-web-service-Hackathon_2022-"
+    },
+    {
+        id: 3,
+        title: "Weather Forecast",
+        description: "A web app providing weather forecasts using a public API.",
+        image: "images/P1.jpg",
+        link: "https://daily-weather-f1625.netlify.app/",
+        github: "https://github.com/ADITYAPIMPALE11127/Weather-Forecast-Website"
+    },
+
+    {
+        id: 5,
+        title: "Health Records",
+        description: "A smart contract for managing health records using blockchain.",
+        image: "images/P5.jpg",
+        github: "https://github.com/ADITYAPIMPALE11127/Blockchain-Based-application-for-health-related-medical-records"
+    },
+    {
+        id: 6,
+        title: "E-Voting System",
+        description: "A Solidity smart contract for secure & transparent e-voting.",
+        image: "images/P6.webp",
+        github: "https://github.com/ADITYAPIMPALE11127/E-Voting-dApp-using-Blockchain"
+    },
+    {
+        id: 7,
+        title: "Quiz Genie - Chat Bot",
+        description: "An interactive chat bot for quizzes, age guessing",
+        image: "images/P7.png",
+        github: "https://github.com/ADITYAPIMPALE11127/Simple-Quiz-Genie-ChatBot"
+    },
+    {
+        id: 8,
+        title: "Volume Control System",
+        description: "Control computer volume using hand gestures",
+        image: "images/P8.png",
+        github: "https://github.com/ADITYAPIMPALE11127/Volume-Control-through-hand-sign-gesture"
+    },
+    {
+        id: 4,
+        title: "Simple Calculator",
+        description: "A basic calculator application built with HTML, CSS, and JavaScript.",
+        image: "images/P9.png",
+        github: "https://github.com/ADITYAPIMPALE11127/Calculator-mini-project"
+    },
+    {
+        id: 9,
+        title: "Face Recognition System",
+        description: "Real-time face recognition using DeepFace with webcam input.",
+        image: "images/P4.jpg",
+        github: "https://github.com/ADITYAPIMPALE11127/livefacerecog"
+    },
+];
+
+export default projectData;
