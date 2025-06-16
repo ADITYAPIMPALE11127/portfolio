4c2c1a6726bed6278d28a3424a0b5b4b15035475
Aditya Pimpale <pimpaleaditya2@gmail.com>
2025-06-16 12:30:13 +0530

    #4

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,9 +1,20 @@
+import P1 from '../../images/P1.jpg';
+import P2 from '../../images/P2.jpg';
+import P3 from '../../images/P3.jpg';
+import P4 from '../../images/P4.jpg';
+import P5 from '../../images/P5.jpg';
+import P6 from '../../images/P6.webp';
+import P7 from '../../images/P7.png';
+import P8 from '../../images/P8.png';
+import P9 from '../../images/P9.png';
+import P10 from '../../images/P10.jpeg';
+
 const projectData = [
     {
         id: 1,
         title: "Snap Search",
         description: "An image gallery app using React and the Pixabay API.",
-        image: "/images/P2.jpg",
+        image: P2,
         link: "https://snappy1635.netlify.app/",
         github: "https://github.com/ADITYAPIMPALE11127/image-gallery-react--1"
     },
@@ -11,7 +22,7 @@ const projectData = [
         id: 2,
         title:"Expense Tracker App",
         description:"Efficiently helps track expenses with Friendly UI.",
-        image:"/images/P10.jpeg",
+        image: P10,
         link: "https://expense-tracking-app-liart.vercel.app/",
         github: "https://github.com/ADITYAPIMPALE11127/expense-tracker"
     },
@@ -19,14 +30,14 @@ const projectData = [
         id: 10,
         title: "Bulk SMS & Email Service",
         description: "A web service for efficient bulk SMS and email delivery.",
-        image: "/images/P3.jpg",
+        image: P3,
         github: "https://github.com/ADITYAPIMPALE11127/Bulk-SMS-and-email-sending-web-service-Hackathon_2022-"
     },
     {
         id: 3,
         title: "Weather Forecast",
         description: "A web app providing weather forecasts using a public API.",
-        image: "/images/P1.jpg",
+        image: P1,
         link: "https://daily-weather-f1625.netlify.app/",
         github: "https://github.com/ADITYAPIMPALE11127/Weather-Forecast-Website"
     },
@@ -34,42 +45,42 @@ const projectData = [
         id: 5,
         title: "Health Records",
         description: "A smart contract for managing health records using blockchain.",
-        image: "/images/P5.jpg",
+        image: P5,
         github: "https://github.com/ADITYAPIMPALE11127/Blockchain-Based-application-for-health-related-medical-records"
     },
     {
         id: 6,
         title: "E-Voting System",
         description: "A Solidity smart contract for secure & transparent e-voting.",
-        image: "/images/P6.webp",
+        image: P6,
         github: "https://github.com/ADITYAPIMPALE11127/E-Voting-dApp-using-Blockchain"
     },
     {
         id: 7,
         title: "Quiz Genie - Chat Bot",
         description: "An interactive chat bot for quizzes, age guessing",
-        image: "/images/P7.png",
+        image: P7,
         github: "https://github.com/ADITYAPIMPALE11127/Simple-Quiz-Genie-ChatBot"
     },
     {
         id: 8,
         title: "Volume Control System",
         description: "Control computer volume using hand gestures",
-        image: "/images/P8.png",
+        image: P8,
         github: "https://github.com/ADITYAPIMPALE11127/Volume-Control-through-hand-sign-gesture"
     },
     {
         id: 4,
         title: "Simple Calculator",
         description: "A basic calculator application built with HTML, CSS, and JavaScript.",
-        image: "/images/P9.png",
+        image: P9,
         github: "https://github.com/ADITYAPIMPALE11127/Calculator-mini-project"
     },
     {
         id: 9,
         title: "Face Recognition System",
         description: "Real-time face recognition using DeepFace with webcam input.",
-        image: "/images/P4.jpg",
+        image: P4,
         github: "https://github.com/ADITYAPIMPALE11127/livefacerecog"
     },
 ];
